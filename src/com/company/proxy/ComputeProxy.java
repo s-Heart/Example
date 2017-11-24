120c9d3d09e73e0f815005e193ee73337a6f566d
史少杰 <shishaojie@koolearn-inc.com>
2017-11-24 15:51:35 +0800

    add readme

--- src/com/company/proxy/ComputeProxy.java
+++ src/com/company/proxy/ComputeProxy.java
@@ -5,7 +5,9 @@ import com.company.task.ComputeTask;
 import com.company.task.TResult;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
@@ -16,37 +18,53 @@ import java.util.concurrent.Executors;
  */
 public class ComputeProxy {
     private final ExecutorService executorService;
-    private int threadFinishNum;
-    private int threadCount;
+    private int taskFinishNum;
+    private int fileSum;
     private File[] files;
     private CallbackListener listener;
-    private Map<String, TResult> sumMap = new ConcurrentHashMap<>();
+    private Map<String, TResult> sumMap = new HashMap<>();
+    private List<Map<String, TResult>> finishList = new ArrayList<>();
 
     public ComputeProxy(String directoryPath) {
         File root = new File(directoryPath);
         if (root.isDirectory()) {
             files = root.listFiles();
-            threadCount = files.length;
-            System.out.println("threadCount==============" + threadCount);
+            fileSum = files.length;
+            System.out.println("fileSum==============" + fileSum);
         }
-        executorService = Executors.newFixedThreadPool(files.length);
-        threadFinishNum = 0;
+        //todo 机械硬盘/SSD 分别测试执行效率
+        executorService = Executors.newFixedThreadPool(1);
+        taskFinishNum = 0;
     }
 
     public void setCallbackListener(CallbackListener callbackListener) {
         this.listener = callbackListener;
     }
 
+    private Object lock=new Object();
     private InnerHandler proxyListener = new InnerHandler() {
         @Override
         public void postResult(Map<String, TResult> threadMap) {
-            threadFinishNum++;
-            doStat(threadMap);
-            if (threadFinishNum == threadCount) {
-                listener.onFinish(sumMap);
-            }
+//            synchronized (lock) {
+                taskFinishNum++;
+                System.out.println("taskFinishNum==========="+ taskFinishNum);
+                finishList.add(threadMap);
+//                doStat(threadMap);
+                if (taskFinishNum == fileSum) {
+                    doStat(finishList);
+//                    listener.onFinish(sumMap);
+                }
+//            }
         }
     };
+
+    private void doStat(List<Map<String, TResult>> finishList) {
+        for (Map<String, TResult> threadMap : finishList) {
+            doStat(threadMap);
+        }
+
+        listener.onFinish(sumMap);
+    }
 
     private void doStat(Map<String, TResult> threadMap) {
         for (String domain : threadMap.keySet()) {
